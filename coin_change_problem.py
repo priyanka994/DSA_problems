4f8be4fd62eb10a6746065ffa306a9a5958f59a9
priyanka994 <priyankavutkur6@gmail.com>
2023-11-22 19:21:05 +0530

    22/11/2023

---
+++ coin_change_problem.py
@@ -0,0 +1,19 @@
+
+def count(coins, n, sum):
+
+	if (sum == 0):
+		return 1
+
+	if (sum < 0):
+		return 0
+
+	if (n <= 0):
+		return 0
+
+	return count(coins, n - 1, sum) + count(coins, n, sum-coins[n-1])
+
+
+coins = [1, 2, 3]
+n = len(coins)
+print(count(coins, n, 5))
+
